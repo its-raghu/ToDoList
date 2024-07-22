ca2573507120a0eee93f5a229c7fc62d57603820
its-raghu <raghava.simhadri@gmail.com>
2024-07-22 07:10:03 +0530

    updated tailwind

--- to-do-list/src/TextArea.jsx
+++ to-do-list/src/TextArea.jsx
@@ -9,7 +9,7 @@ function TextArea (props){
     }
 
   return (
-    <div>
+    <div className='text-center'>
         <input className='w-[70%] bg-transparent border-b-4 border-black text-[black] border-dashed m-[1rem] p-[0.50rem] text-[1.5rem] font-[400]'
          type="text" value={inputText} onChange={handleChange}/>
 
